95ebadfe42fbe7289c369c39e507ac1166e7b0d7
TychonK <78542451+TychonK@users.noreply.github.com>
2022-03-03 08:06:49 +0000

    Deploying to gh-pages from @ TychonK/parcel-starter-kit@a99fa6813eebdb9f714dfe4381880cddc8b4435e 🚀

---
+++ index.55b74f05.js
@@ -0,0 +1,2 @@
+!function(){console.log("hello world!");var o=new Date;console.log(o)}();
+//# sourceMappingURL=index.55b74f05.js.map
---
+++ index.dbb60925.js
@@ -0,0 +1,2 @@
+console.log("hello world!");const o=new Date;console.log(o);
+//# sourceMappingURL=index.dbb60925.js.map
